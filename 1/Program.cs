20c03673cece6b395389aaff9741bee191fff13e
Danik Danik <danilprov2004@gmail.com>
2021-10-20 19:45:54 +0300

    Ex.1

--- 1/Program.cs
+++ 1/Program.cs
@@ -10,6 +10,23 @@ namespace _1
     {
         static void Main(string[] args)
         {
+            char liter;
+            int space = 0;
+            string text="";
+            Console.WriteLine("Hello, please, enter your text: ");
+            do
+            {
+                liter = Console.ReadKey().KeyChar;
+                text += liter;
+                if(liter == ' ')
+                {
+                    space++;
+                }
+                
+
+            } while (liter != '.');
+            Console.WriteLine("Your string end. You have " + space + "speces. Your string: " + text);
+            Console.Read();
         }
     }
 }
