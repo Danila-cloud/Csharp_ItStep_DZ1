85d447d8b1ed4739b39bfda47b15d5cefe7c3f46
Danik Danik <danilprov2004@gmail.com>
2021-10-20 23:52:26 +0300

    Ex.3

--- 3/Program.cs
+++ 3/Program.cs
@@ -10,6 +10,79 @@ namespace _3
     {
         static void Main(string[] args)
         {
+
+            int symbol;
+            char result;
+
+
+            Console.WriteLine("Please, enter your symbol: ");
+            do
+            {
+                
+                symbol = Console.Read();
+                //if (symbol < 65 && symbol > 90 || symbol < 97 && symbol > 122)
+                //{
+                //    Console.WriteLine("Please, enter corect form!!!");
+                //}
+                //if(symbol < 65)
+                //{
+                //    Console.WriteLine("Please, enter corect form!!!");
+                //}
+                //else if(symbol > 90 && symbol < 97)
+                //{
+                //    Console.WriteLine("Please, enter corect form!!!");
+                //}
+                //else if(symbol > 122)
+                //{
+                //    Console.WriteLine("Please, enter corect form!!!");
+                //}
+                if (symbol < 97 && symbol > 122)
+                {
+                    Console.WriteLine("Erore!!!");
+                }
+                else if (symbol < 65 && symbol > 90)
+                {
+                    Console.WriteLine("Erore!!!");
+                }
+                if (symbol >= 65 && symbol <= 90)
+                {
+                    symbol += 32;
+                    result = (char)symbol;
+                    Console.WriteLine(result);
+
+                }
+                else if (symbol >= 97 && symbol <= 122)
+                {
+                    symbol -= 32;
+                    result = (char)symbol;
+                    Console.WriteLine(result);
+                }
+
+                //else
+                //{
+                //    Console.WriteLine("Please, enter corect form!!!");
+                //}
+
+
+            } while (true);
+
+
+
+
+
+            //symbol = Console.Read();
+
+            //str = Console.ReadLine();
+            //char[] num = str.ToCharArray();
+            //for (int i = 0; i < num.Length ; i++)
+            //{
+            //    symbol = int.Parse(num[i]);
+            //}
+
+
+
+
+            Console.Read();
         }
     }
 }
