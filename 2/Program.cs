bc19349b703b9d8bb2ff4d39430dd131c621b789
Danik Danik <danilprov2004@gmail.com>
2021-10-20 16:05:22 +0300

    Ex.2

--- 2/Program.cs
+++ 2/Program.cs
@@ -10,6 +10,35 @@ namespace _2
     {
         static void Main(string[] args)
         {
+            int num, figure, k = 0, firstpass = 0, secondpass = 0;
+            Console.WriteLine("HEllo, please give me your numeric:");
+            do
+            {
+                figure = Console.ReadKey().KeyChar;
+                num = int.Parse(figure.ToString());
+                k++;
+                if (k <= 3)
+                {
+                    firstpass += num;
+                }
+                else
+                {
+                    secondpass += num;
+                }
+            } while (k!=6);
+            if(firstpass == secondpass)
+            {
+                Console.WriteLine(" !" +
+                    "your ticket is happy!");
+            }
+            else
+            {
+                Console.WriteLine(" !" +
+                    "your ticket is not happy!");
+            }
+
+
+            Console.Read();
         }
     }
 }
